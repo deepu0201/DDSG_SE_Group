6bcb58d37caad3e5c35632f172abe15e88e6d00f
deydaniel <deydaniel00@gmail.com>
2021-03-11 21:12:29 +0530

    Partially Finished Login In screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Screen'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/adminScreen');
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
+                    horizontal: 80.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  side: BorderSide(color: Colors.lightBlue[800]),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/adminScreen');
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
